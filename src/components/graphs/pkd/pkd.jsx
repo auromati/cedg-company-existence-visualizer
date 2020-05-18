86c52315d7cd44a2cb2704574782abbc7d232349
Karol Ulanowski <ulanowskikarol@gmail.com>
2020-05-18 23:45:59 +0200

    fix pkds

--- src/components/graphs/pkd/pkd.jsx
+++ src/components/graphs/pkd/pkd.jsx
@@ -10,12 +10,7 @@ export function Pkd({ selected }) {
     const survivalData = useContext(SurvivalDataContext);
     const [histogramData, setHistogramData] = useState([]);
     useEffect(() => {
-        const section = selected.code.length === 1 ? selected.code.substring(0, 1) : null;
-        const division = selected.code.length === 3 ? Number(selected.code.substring(1, 3)) : null;
-        const group = selected.code.length === 4 ? Number(selected.code.substring(1, 4)) : null;
-        const pkdClass = selected.code.length === 5 ? Number(selected.code.substring(1, 5)) : null;
-
-        setHistogramData(readPkdData(survivalData, section, division, group, pkdClass));
+        setHistogramData(readPkdData(survivalData, selected.code));
     }, [survivalData, selected]);
     return (
         <BarChart
