28c5bcecf39daabbeac9c2a6c7b4d4d6c542f293
Damian <damian.rakus@gmail.com>
2020-05-19 13:25:54 +0200

    Percentage based results

--- src/components/graphs/numOfPlaces/places-data-reader.js
+++ src/components/graphs/numOfPlaces/places-data-reader.js
@@ -19,7 +19,7 @@ export function readPlacesData(survivalData, monthsPerBin = 5 ) {
         }
     }
 
-    return map(data, (numOfPlacesCount, monthWindow) => ({
+    data = map(data, (numOfPlacesCount, monthWindow) => ({
         'month': monthsPerBin === 1 ? monthWindow : `${monthWindow*monthsPerBin}-${(parseInt(monthWindow) + 1) * monthsPerBin - 1}`,
         '1': numOfPlacesCount[0] || 0,
         '2': numOfPlacesCount[1] || 0,
@@ -28,4 +28,33 @@ export function readPlacesData(survivalData, monthsPerBin = 5 ) {
         '5': numOfPlacesCount[4] || 0,
         '6+': numOfPlacesCount[5] || 0
     }));
+    
+    let sumOfValues = {
+        '1': 0,
+        '2': 0,
+        '3': 0,
+        '4': 0,
+        '5': 0,
+        '6+': 0
+    }
+
+    for(let dataDict of data) {
+        sumOfValues['1'] += dataDict['1'];
+        sumOfValues['2'] += dataDict['2'];
+        sumOfValues['3'] += dataDict['3'];
+        sumOfValues['4'] += dataDict['4'];
+        sumOfValues['5'] += dataDict['5'];
+        sumOfValues['6+'] += dataDict['6+'];
+    }
+    for(let dataDict of data) {
+        dataDict['0'] = 100 * dataDict['0'] / sumOfValues['0'];
+        dataDict['1'] = 100 * dataDict['1'] / sumOfValues['1'];
+        dataDict['2'] = 100 * dataDict['2'] / sumOfValues['2'];
+        dataDict['3'] = 100 * dataDict['3'] / sumOfValues['3'];
+        dataDict['4'] = 100 * dataDict['4'] / sumOfValues['4'];
+        dataDict['5'] = 100 * dataDict['5'] / sumOfValues['5'];
+        dataDict['6+'] = 100 * dataDict['6+'] / sumOfValues['6+'];
+    }
+
+    return data;
 }
