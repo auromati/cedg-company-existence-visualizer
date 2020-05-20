ac329337789f4f1075ee7f8dbce6ee845719e59f
Damian <damian.rakus@gmail.com>
2020-05-20 12:01:40 +0200

    Spinner for map

--- src/components/graphs/polandMap/polandMapContainer.jsx
+++ src/components/graphs/polandMap/polandMapContainer.jsx
@@ -3,6 +3,7 @@ import { SurvivalDataContext } from '../../../App';
 import { readPolandData, readMedian } from "./poland-data-reader";
 import { PolandMap } from './polandMap';
 import { Row, Col } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 
 
 export const PolandMapContainer = () => {
@@ -15,15 +16,26 @@ export const PolandMapContainer = () => {
         setMedians(readMedian(survivalData));
     }, [survivalData]);
 
+    return (<div style={{textAlign: 'center', margin: '20px auto'}}>
+        {
+            data && data.length ? (<div>
+                <h1>Przeanalizuj, gdzie umieścić swoją firmę</h1>
+                    Najwięcej miesięcy przeżywają firmy z województwa mazowieckiego, najkrócej z kujawsko-pomorskiego i warmińsko-mazurskiego
+                <Row>
+                <Col xs={5}>
+                    <PolandMap data={data} medians={medians}></PolandMap>
+                </Col>
+            </Row>
+            </div>) :
+            <Spinner animation="border" role="status" variant="success">
+                <span className="sr-only">Loading...</span>
+            </Spinner>
+        }
+    </div>);;
+
     return (
     <div>
-        <h1>Przeanalizuj, gdzie umieścić swoją firmę</h1>
-        Najwięcej miesięcy przeżywają firmy z województwa mazowieckiego, najkrócej z kujawsko-pomorskiego i warmińsko-mazurskiego
-        <Row>
-            <Col xs={5}>
-                <PolandMap data={data} medians={medians}></PolandMap>
-            </Col>
-        </Row>
+        
     </div>)
 };
 
